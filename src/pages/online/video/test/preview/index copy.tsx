729365350933cf49a34b26aea17cfbb5fa908470
le trung <trung.le@truenorth.edu.vn>
2022-04-14 13:34:38 +0700

    change facing

--- src/pages/online/video/test/preview/index copy.tsx
+++ src/pages/online/video/test/preview/index copy.tsx
@@ -15,8 +15,10 @@ import {
 const OnlineVideoTestPage: React.FC = () => {
     const [isRunning, setRunning] = useState(false)
     const [count, setCount] = useState(0)
-    const videoRef = useRef(null);
-  const photoRef = useRef(null);
+    const [imageDataURL, setImageDataURL] = useState<any>()
+    const [cameraNumber, setCameraNumber] = useState<any>(0)
+    const player = useRef<any>()
+    let navigator = window.navigator as any;
     useEffect(() => {
         if (isRunning) {
             let myInterval = setInterval(() => {
@@ -41,37 +43,111 @@ const OnlineVideoTestPage: React.FC = () => {
         }, 1000)
     }, [])
     useEffect(() => {
-      getVideo();
-    }, [videoRef]);
+        // initializeMedia()
+    }, [window.navigator])
     
 
-    const getVideo = () => {
-      navigator.mediaDevices
-        .getUserMedia({ video: { width: 300 } })
-        .then(stream => {
-          let video:any = videoRef.current;
-          video.srcObject = stream;
-          video.play();
-        })
-        .catch(err => {
-          console.error("error:", err);
+    const initializeMedia = async () => {
+        setImageDataURL(null)
+        console.log(window.navigator);
+        
+        if (!("mediaDevices" in window.navigator)) {
+          (window.navigator as any).mediaDevices = {};
+        }
+    
+        if (!("getUserMedia" in window.navigator.mediaDevices)) {
+            console.log(window.navigator.mediaDevices);
+          window.navigator.mediaDevices.getUserMedia = (constraints: any) => {
+            var getUserMedia =
+            (window.navigator as any).webkitGetUserMedia || (window.navigator as any).mozGetUserMedia;
+    
+            if (!getUserMedia) {
+              return Promise.reject(new Error("getUserMedia Not Implemented"));
+            }
+    
+            return new Promise((resolve, reject) => {
+              getUserMedia.call(window.navigator, constraints, resolve, reject);
+            });
+          };
+        }
+    
+        //Get the details of video inputs of the device
+        const videoInputs = await getListOfVideoInputs();
+        console.log(videoInputs);
+        
+        //The device has a camera
+        if (videoInputs.length) {
+          window.navigator.mediaDevices
+            .getUserMedia({
+              video: {
+                deviceId: {
+                  exact: videoInputs[cameraNumber].deviceId,
+                },
+              },
+            })
+            .then((stream: any) => {
+              player.current.srcObject = stream;
+            })
+            .catch((error: any) => {
+              console.error(error);
+            });
+        } else {
+          alert("The device does not have a camera");
+        }
+      };
+    
+      const capturePicture = () => {
+        var canvas = document.createElement("canvas");
+        canvas.width = player.current.videoWidth;
+        canvas.height = player.current.videoHeight;
+        var contex: any = canvas.getContext("2d");
+        contex.drawImage(player.current, 0, 0, canvas.width, canvas.height);
+        player.current.srcObject.getVideoTracks().forEach((track: any) => {
+          track.stop();
         });
-    };
-  
-    const paintToCanvas = () => {
-      let video = videoRef.current;
-      let photo: any = photoRef.current;
-      let ctx = photo.getContext("2d");
-  
-      const width = 320;
-      const height = 240;
-      photo.width = width;
-      photo.height = height;
-  
-      return setInterval(() => {
-        ctx.drawImage(video, 0, 0, width, height);
-      }, 200);
-    };
+    
+        console.log(canvas.toDataURL());
+        setImageDataURL(canvas.toDataURL())
+      };
+    
+      const switchCamera = async () => {
+        const listOfVideoInputs = await getListOfVideoInputs();
+    
+        // The device has more than one camera
+        if (listOfVideoInputs.length > 1) {
+          if (player.current.srcObject) {
+            player.current.srcObject.getVideoTracks().forEach((track: any) => {
+              track.stop();
+            });
+          }
+    
+          // switch to second camera
+          if (cameraNumber === 0) {
+            setCameraNumber(1)
+          }
+          // switch to first camera
+          else if (cameraNumber === 1) {
+            setCameraNumber(0)
+          }
+    
+          // Restart based on camera input
+          initializeMedia();
+        } else if (listOfVideoInputs.length === 1) {
+          alert("The device has only one camera");
+        } else {
+          alert("The device does not have a camera");
+        }
+      };
+    
+      const getListOfVideoInputs = async () => {
+        // Get the details of audio and video output of the device
+        console.log(window.navigator.mediaDevices);
+        
+        const enumerateDevices = await window.navigator.mediaDevices.enumerateDevices();
+    
+        //Filter video outputs (for devices with multiple cameras)
+        return enumerateDevices.filter((device: any) => device.kind === "videoinput");
+      };
 
     return (
         <>
@@ -112,14 +188,12 @@ const OnlineVideoTestPage: React.FC = () => {
                     </OnlineVideoPreview>
                     <OnlineVideoCamera>
                         <video
-                            // ref={videoRef}
-                            autoPlay
-                            playsInline
-                            muted
-                        ></video>
+                          ref={player}
+                          autoPlay
+                      ></video>
                     </OnlineVideoCamera>
                     <OnlineVideoNav>
-                        <li>
+                        <li onClick={() => initializeMedia()}>
                             <VideoNavButton
                                 href="#"
                                 onClick={(e) => {
@@ -130,6 +204,7 @@ const OnlineVideoTestPage: React.FC = () => {
                             </VideoNavButton>
                         </li>
                         <li
+                            onClick={() => switchCamera()}
                         >
                             <VideoNavButton
                                 href="#"
@@ -144,8 +219,7 @@ const OnlineVideoTestPage: React.FC = () => {
                         </li>
                     </OnlineVideoNav>
                 </OnlineVideo>
-                <video onCanPlay={() => paintToCanvas()} ref={videoRef} />
-                <canvas ref={photoRef} />
+                
             </DefaultLayout>
         </>
     )
